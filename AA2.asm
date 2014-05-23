340124890c7b236036c6a30e386777783cba2024
ZeeshanAK <prince_xeeshan@hotmail.com>
2014-05-23 10:14:26 +0500

    Create AA2.asm

---
+++ AA2.asm
@@ -0,0 +1,16 @@
+Question:
+
+Write an assembly language code to write subroutines for the following two binomial theorems:
+
+SROUTINE_SUM:	 SUM_SQ = (A+B)2 = A2 + B2 + 2*A*B
+
+SROUTINE_DIF:	 DIF_SQ = (A-B)2 = A2 + B2 - 2*A*B
+
+
+Where SUM_SQ, DIF_SQ, A and B are predefined data labels while SROUTINE_SUM and SROUTINE_DIF are the subroutine names.
+
+Testing values for A and B are 3 and 2 respectively.
+
+You have to attach the snapshot of AFD debugger showing the calculated values in SUM_SQ and DIF_SQ.
+
+
