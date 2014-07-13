d084bb365d78af40250f0f17f6cf6836ce667a65
ZeeshanAK <zeeshankhan.a1@gmail.com>
2014-07-13 22:36:57 +0500

    Adding RecursiveFunction to Examples.

---
+++ Examples/RecursiveFunction.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int FunCounter = 0;
+void recursiveFun()
+{
+    FunCounter++;
+    if( FunCounter <=5)
+    cout << "Recursive call number: " << FunCounter << endl;
+    return recursiveFun();
+
+}
+
+
+int main()
+{
+    recursiveFun();
+}
