92e74093fc2f1804b21ebfbc16f01f7114b9418d
ZeeshanAK <zeeshankhan.a1@gmail.com>
2014-07-13 21:38:35 +0500

    Adding Factorial example.

---
+++ Examples/Factorial.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Enter the number to check its Factorial:" <<endl;
+    int number;
+    cin >> number;
+
+    int factorial = 1;
+    for (int i = number; i >= 1; i--){
+        factorial *= i;
+    }
+
+    cout << "Factorial of the number " << number << " is " <<factorial;
+}
