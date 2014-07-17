4cf8d9d12e7124d59fc0dd2672c9fca173f7a801
ZeeshanAK <zeeshankhan.a1@gmail.com>
2014-07-18 02:02:38 +0500

    Fixed the logic on PollAnalysis

--- Examples/PollAnalysis.cpp
+++ Examples/PollAnalysis.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 int main()
 {
-    const int freqSize= 5;
-    const int freq = 10;
+    const int respSize= 10;
+    const int freqSize = 6;
 
-    int response[freq] = {2,2,3,5,2,2,3,5,2,6};
+    int response[respSize] = {2,2,3,5,2,3,3,5,2,4};
     int frequency[freqSize] = {0};
 
-    for (int i =0; i < freq; i++)
-     frequency[response[i]]++;
+    for (int i =0; i < respSize; i++)
+     frequency[ response[ i ] ]++;
 
-    for (int j = 1; j <= freq; j++)
+    for (int j = 1; j < freqSize; j++)
     {
         cout << j << "\t" << frequency[j] << endl;
     }
