dfbd562809523a433360c1baede0806dfde1fa3e
ZeeshanAK <zeeshankhan.a1@gmail.com>
2014-08-02 02:32:55 +0500

    Adding VariableScope to Examples.

---
+++ Examples/VariableScope.cpp
@@ -0,0 +1,30 @@
+//This program shows how to access variables from different scopes.
+
+#include<iostream>
+using namespace std;
+
+int x = 4;
+
+class test
+{
+public:
+    void dispVar()
+    {
+        int x = 5;
+        cout << "Value of Global X = " << ::x << endl;
+        cout << "Value of function's X = " << x << endl;
+        cout << "Value of Class's X = " << test::x;
+    }
+
+private:
+    int x = 6;
+
+};
+
+
+int main()
+{
+    int x = 3;
+    test testx;
+    testx.dispVar();
+}
