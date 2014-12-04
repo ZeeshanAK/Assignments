697fb462cdd136d457172a974eddfeef6719b625
ZeeshanAK <zeeshankhan.a1@gmail.com>
2014-12-05 00:16:50 +0500

    Fixed the same number issue, see the comments

--- CrapsGame/CrapsGame.cpp
+++ CrapsGame/CrapsGame.cpp
@@ -1,3 +1,11 @@
+/*
+ISSUE: http://stackoverflow.com/questions/27295655/generating-different-random-number-in-second-call?noredirect=1#comment43056907_27295655
+Status: Fixed.
+Resolution: Seed rand once, if you do it twice at the same time, it will generate the same number.
+Also call the rand() once you've seeded it.
+Was calling the rand() before the seed and it kept producing 6 + 6 every time LOL xD
+*/
+
 #include<iostream>
 #include<ctime>
 #include <cstdlib>
@@ -7,7 +15,6 @@ using namespace std;
 //Generating two rand numbers from 1 to 6
 int rollDice()
 {
-    srand(time(0));
     int face1 = 1 + rand()%6;
     int face2 = 1 + rand()%6;
     int sum = face1 + face2;
@@ -16,12 +23,15 @@ int rollDice()
     return sum;
 }
 
+
 string gameStatus; //Hold status of game; WIN, CONTINUE, LOST
-int sumOfDice = rollDice();
+
 int point = 0; //This will hold sum of dice if it's default case defined below in Switch.
 
 int main()
 {
+    srand(time(NULL));
+    int sumOfDice = rollDice();
      switch(sumOfDice)
      {
          case 7:
@@ -54,3 +64,5 @@ int main()
      if (gameStatus == "LOST")
         cout << "Player lost!";
 }
+
+
