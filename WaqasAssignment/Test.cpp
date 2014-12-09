9830a24a665ebb9b8144982f8ff5e81c4fd0ff5a
ZeeshanAK <zeeshankhan.a1@gmail.com>
2014-12-09 16:47:00 +0500

    Almost done. Need to polish

--- WaqasAssignment/Test.cpp
+++ WaqasAssignment/Test.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 void selectLevel();
 void generateSecretNumber(int );
-void guessSecretNumber();
+void guessSecretNumber(int);
 int secretNumber = 0;
 int guessNumber = 0;
 int main()
 {
     srand (time(NULL));
     selectLevel();
-    cout << "Secret number is: " << secretNumber;
+    cout << "\n\nSecret number is: " << secretNumber;
 
 }
 
@@ -28,51 +28,75 @@ void selectLevel()
     {
         if (choice == 1)
         {
-            cout<< "Entering beginner level";
             generateSecretNumber(1);
             break;
         }
-
         else if (choice == 2)
         {
-            cout<< "Entering intermediate   level";
-            generateSecretNumber(2);
+           generateSecretNumber(2);
             break;
         }
-
-
         else if (choice == 3)
         {
             cout<< "Entering Advance  level";
             generateSecretNumber(3);
             break;
-
         }
         else  cout <<"Your choice is incorrect. Please enter again" << endl;
-        cout <<"Choice: ";
+        cout <<"\nChoice: ";
         cin >> choice;
     }
     while(choice != 1 ||choice != 2|| choice!= 3 );
 }
 
-void generateSecretNumber(int );void generateSecretNumber(int );
 void generateSecretNumber(int level)
 {
-
     if (level == 1)
     {
-        cout<< "\n INSIDE beginner level";
+        cout<<"Please guess the secret number in the range [1 - 20]: ";
         secretNumber = 1 + rand()%20 ;
+        guessSecretNumber(3);
     }
 
     if (level == 2)
     {
+         cout<<"Please guess the secret number in the range [1 - 50]: ";
         secretNumber = 1 + rand()%50 ;
+        guessSecretNumber(4);
     }
 
     if (level == 3)
     {
+         cout<<"Please guess the secret number in the range [1 - 100]: ";
         secretNumber = 1 + rand()%100 ;
+        guessSecretNumber(5);
     }
-
 }
+
+void guessSecretNumber(int tries)
+{
+int i = 1;
+
+    while ( i <= tries)
+    {
+        cin >> guessNumber;
+
+
+        if ( (guessNumber > secretNumber) && (i != tries) )
+        {
+            cout << "your entered number is high, please try again: ";
+        }
+        else if  ((guessNumber < secretNumber) && (i != tries) )
+        {
+            cout << "your entered number is low, please try again: ";
+        }
+        else if  (guessNumber == secretNumber)
+        {
+            cout<<"Congratulations!...You won!"<<endl;
+            break;
+        }
+
+        i++;
+    }
+}
+
