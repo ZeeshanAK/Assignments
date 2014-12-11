2d1173f717e7a58b3f234ad217b56288e0fd79d3
ZeeshanAK <zeeshankhan.a1@gmail.com>
2014-12-11 16:30:43 +0500

    Polished. Final.

--- WaqasAssignment/Test.cpp
+++ WaqasAssignment/Test.cpp
@@ -3,79 +3,99 @@
 #include<ctime>
 using namespace std;
 
+
+//Function declaration
 void selectLevel();
 void generateSecretNumber(int );
 void guessSecretNumber(int);
+
+//These will hold secret and guess numbers
 int secretNumber = 0;
 int guessNumber = 0;
+
+
 int main()
 {
-    srand (time(NULL));
+    srand (time(NULL)); //  seeding the random number
+
+    //Transferring control to Select Level
     selectLevel();
-    cout << "\n\nSecret number is: " << secretNumber;
+
+    cout << "\nSecret number was: " << secretNumber;
 
 }
 
 void selectLevel()
 {
+    //This will hold user choice which we will define the range for random numbers
     int choice;
+
     cout << "Enter 1 for beginner level" << endl;
     cout << "Enter 2 for Intermediate level" << endl;
-    cout << "Enter 3 for advance level" << endl;
+    cout << "Enter 3 for Advance level" << endl;
+    cout << "\nChoice: " ;
     cin >> choice;
 
+    //Loop until user inputs a correct choice
     do
     {
+        //Calling beginner level
         if (choice == 1)
         {
             generateSecretNumber(1);
             break;
         }
+
+        //Calling Intermediate level
         else if (choice == 2)
         {
            generateSecretNumber(2);
             break;
         }
+
+        //Calling Advance  level
         else if (choice == 3)
         {
-            cout<< "Entering Advance  level";
             generateSecretNumber(3);
             break;
         }
         else  cout <<"Your choice is incorrect. Please enter again" << endl;
         cout <<"\nChoice: ";
         cin >> choice;
-    }
-    while(choice != 1 ||choice != 2|| choice!= 3 );
+    } while(choice != 1 ||choice != 2|| choice!= 3 );
 }
 
+
+//Function to generate random numbers depending on the level which we get as argument
 void generateSecretNumber(int level)
 {
     if (level == 1)
     {
+        secretNumber = 1 + rand()%20 ;  //Generating numbers from 1-20
         cout<<"Please guess the secret number in the range [1 - 20]: ";
-        secretNumber = 1 + rand()%20 ;
         guessSecretNumber(3);
     }
 
     if (level == 2)
     {
-         cout<<"Please guess the secret number in the range [1 - 50]: ";
-        secretNumber = 1 + rand()%50 ;
+        secretNumber = 1 + rand()%50 ;  //Generating numbers from 1-50
+        cout<<"Please guess the secret number in the range [1 - 50]: ";
         guessSecretNumber(4);
     }
 
     if (level == 3)
     {
-         cout<<"Please guess the secret number in the range [1 - 100]: ";
-        secretNumber = 1 + rand()%100 ;
+        secretNumber = 1 + rand()%100 ;    //Generating numbers from 1-100
+        cout<<"Please guess the secret number in the range [1 - 100]: ";
         guessSecretNumber(5);
     }
 }
 
+//This function compares the user input with secret numbers.
+//We receive the number of tries as argument
 void guessSecretNumber(int tries)
 {
-int i = 1;
+    int i = 1; //To be used in loop for number of tries
 
     while ( i <= tries)
     {
@@ -95,7 +115,6 @@ int i = 1;
             cout<<"Congratulations!...You won!"<<endl;
             break;
         }
-
         i++;
     }
 }
